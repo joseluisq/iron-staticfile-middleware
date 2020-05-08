59702bc15cf6d11e96491ae1101aeedcaafbed41
Jose Quintana <joseluisquintana20@gmail.com>
2020-05-08 03:08:23 +0200

    refactor: minor updates

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,7 +3,7 @@ extern crate iron;
 extern crate playground_middleware;
 
 use iron::prelude::*;
-use playground_middleware::{Staticfile, Prefix, ModifyWith, Cache, GuessContentType};
+use playground_middleware::{Cache, GuessContentType, ModifyWith, Prefix, Staticfile};
 use std::time::Duration;
 
 const ADDRESS: &'static str = "127.0.0.1:8000";
@@ -16,13 +16,16 @@ fn main() {
 
     let one_day = Duration::new(60 * 60 * 24, 0);
     let one_year = Duration::new(60 * 60 * 24 * 365, 0);
-    let default_content_type = "text/html".parse()
+    let default_content_type = "text/html"
+        .parse()
         .expect("Unable to create default content type");
 
     files.link_after(ModifyWith::new(Cache::new(one_day)));
     files.link_after(Prefix::new(&["assets"], Cache::new(one_year)));
     files.link_after(GuessContentType::new(default_content_type));
 
-    let _server = Iron::new(files).http(ADDRESS).expect("Unable to start server");
+    let _server = Iron::new(files)
+        .http(ADDRESS)
+        .expect("Unable to start server");
     println!("Server listening at {}", ADDRESS);
 }
